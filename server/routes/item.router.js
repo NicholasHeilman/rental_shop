851737ceb083ce82b1fc65d2d592227cadd53f5d
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-07-23 13:29:55 -0500

    add react-table dependencie

---
+++ server/routes/item.router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+//routes will go here
+
+module.exports = router;
---
+++ src/Table/Table.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Table extends Component {
+    render() {
+        return (
+            <div>
+                <p>This wil be a table</p>
+            </div>
+        );
+    }
+}
+
+export default Table;
